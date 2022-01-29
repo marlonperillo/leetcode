3a8008069a6959a67121a91d02166fc7c2a4b198
marlon <marlonp@peplink.com>
2022-01-29 16:38:24 -0600

    Add solution to Reverse String

---
+++ problems/reverseString.js
@@ -0,0 +1,6 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+// eslint-disable-next-line no-unused-vars
+const reverseString = (s) => s.reverse();
