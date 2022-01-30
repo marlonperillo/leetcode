b64d6acd8bc4e7c44ac99e6ac63ff803cf69d50d
marlon <marlonp@peplink.com>
2022-01-30 16:02:07 -0600

    Add solution to fizz buzz

---
+++ problems/fizzBuzz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = (n) => {
+  const returnArray = [];
+  for (let i = 1; i < 20000; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      returnArray.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      returnArray.push('Fizz');
+    } else if (i % 5 === 0) {
+      returnArray.push('Buzz');
+    } else {
+      returnArray.push(`${i}`);
+    }
+  }
+  return returnArray.slice(0, n);
+};
